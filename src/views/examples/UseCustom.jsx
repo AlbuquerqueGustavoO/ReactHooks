84d26c6f5636b6c159c117574814a341118b60e3
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2021-07-22 19:35:32 -0300

    hooks conceitos e exercicios

---
+++ src/views/examples/UseCustom.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useCounter } from '../../components/hooks/useCounter'
+import { useFetch } from '../../components/hooks/UseFetch'
+import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const UseRef = (props) => {
+
+    // fetch ('http://files.cod3r.com.br/curso-react/estados.json')
+    // .then(resp => resp.json())
+    // .then(json => console.log(json))
+
+
+    const [count, inc, dec] = useCounter(10)
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+    const response = useFetch(url)
+
+    function showStates(states) {
+        return states.map(state => <li key={state.nome}>{state.nome} - {state.sigla}</li>)
+    }
+
+    return (
+        <div className="UseCustom">
+            <PageTitle
+                title="Seu Hook"
+                subtitle="Vamos aprender como criar o nosso próprio Hook!"
+            />
+
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+            </div>
+            <div>
+                <button className="btn"
+                    onClick={() => dec()}>-1</button>
+                <button className="btn"
+                    onClick={() => inc()}>+1</button>
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <ul>
+                    {!response.loading ? showStates(response.data) : false}
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default UseRef
